a0335d29a9bf0cf770ea28f3a6d96545b19a2faf
DarkYT <patelspandan8260@gmail.com>
2024-11-25 17:55:22 +0530

    structures in c

---
+++ C/string/quiz.py
@@ -0,0 +1,5 @@
+def fun(a,b):
+    c=a+b
+    return c
+y=fun(4,5)
+print(y)
---
+++ C/structure/cls1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+ int main(){ 
+  struct abc{
+    int slno;
+    char name[30];
+    float salary;
+  }a;
+  struct abc b;
+  a.slno=1;
+  strcpy(a.name," spandan patel ");
+  a.salary= 23333.3456;
+  b.slno=2;
+//   b.name="saurya";
+  b.salary=546343.454;
+//   printf("%d",a);
+  printf(" \n %d %s %f \n",a.slno,a.name,a.salary);
+//   puts(a.name);
+
+return 0;
+}
